1ea1e308c87b0b541e410b1cce62252a671c66fa
David Izhaki <134300714+DavidIzhaki@users.noreply.github.com>
2024-06-24 21:11:15 +0300

    Delete .history directory

--- .history/routes/videosRoutes_20240624152424.js
+++
@@ -1,9 +0,0 @@
-// routes/videoRoutes.js
-const express = require('express');
-const router = express.Router();
-const videoController = require('../controllers/videoController');
-
-router.get('/', videoController.getVideos);
-router.post('/', videoController.createVideo);
-
-module.exports = router;
